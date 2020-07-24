ebef4a838f540502dfbb515c77bd01c0296eccdf
DanielGuti14 <68741814+DanielGuti14@users.noreply.github.com>
2020-07-24 16:15:53 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "nat" {
-  ami                         = "ami-0ba1c3ef9156479a2"
-  availability_zone           = "eu-west-1a"
+  ami                         = "ami-0eec727d3bec65ade"
+  availability_zone           = "ap-southeast-1"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
   vpc_security_group_ids      = [aws_security_group.nat.id]
@@ -9,13 +9,13 @@ resource "aws_instance" "nat" {
   source_dest_check           = false
 
   tags = {
-    Name = "VPC NAT"
+    Name = "VPC NAT - DGM"
   }
 }
 
 resource "aws_instance" "web-1" {
   ami                         = var.amis[var.aws_region]
-  availability_zone           = "eu-west-1a"
+  availability_zone           = "ap-southeast-1a"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
   vpc_security_group_ids      = [aws_security_group.web.id]
@@ -24,22 +24,22 @@ resource "aws_instance" "web-1" {
   source_dest_check           = false
 
   tags = {
-    Name = "Web Server 1"
+    Name = "Web Server 1 - DGM"
   }
 }
 
 resource "aws_instance" "db-1" {
   ami                    = var.amis[var.aws_region]
-  availability_zone      = "eu-west-1a"
+  availability_zone      = "ap-southeast-1a"
   instance_type          = "t2.micro"
   key_name               = var.aws_key_name
   vpc_security_group_ids = [aws_security_group.db.id]
-  subnet_id              = aws_subnet.eu-west-1a-private.id
+  subnet_id              = aws_subnet.ap-southeast-1a-private.id
   associate_public_ip_address = true
   source_dest_check      = false
 
   tags = {
-    Name = "DB Server 1"
+    Name = "DB Server 1 - DGM"
   }
 }
 
